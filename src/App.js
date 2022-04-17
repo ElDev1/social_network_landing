d036948b673a165ca0ae350dd346e173c221ce0a
Devi Amaolo <deviamaolo@gmail.com>
2022-04-17 02:32:05 -0300

    testing mui components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { Settings } from "@mui/icons-material";
+import { Button, styled, Typography } from "@mui/material";
 
 function App() {
+  const BlueButton = styled(Button)(({theme})=> ({
+    backgroundColor: theme.palette.otherColor.main,
+    color: "#888",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue"
+    },
+    "&disabled": {
+      backgroundColor: "gray",
+      color: "white"
+    }
+  }))
+  
   return (
-    <div className="App">
-      Hello
+    <div>
+      <Button variant="text">text</Button>
+      <Button
+        startIcon={<Settings/>}
+        variant="contained"
+        color="otherColor"
+        size="small" 
+      >
+        Settings
+      </Button>
+      <BlueButton>My Button</BlueButton>
     </div>
   );
 }
